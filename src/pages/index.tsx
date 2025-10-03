e9776c403dbe4f61d151734988ef6b5294db0f34
Name <fasyaraihan82@gmail.com>
2025-10-03 18:42:28 +0700

    whehehe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-// import { button as buttonStyles } from "@heroui/theme";
-import {Image} from "@heroui/image";
-import zigma from "@/assets/IMG-20250314-WA0011.png"
-
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
@@ -14,13 +10,7 @@ export default function IndexPage() {
           <span className={title({ color: "violet" })}>Raihan&nbsp;</span>
           <span className={title()}>Maulana</span>
         </div>
-        <Image
-          isBlurred
-          alt="HeroUI Album Cover"
-          className="m-5"
-          src={zigma}
-          width={240}
-        />
+        
       </section>
     </DefaultLayout>
   );
